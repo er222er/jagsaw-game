ad507826f79c5a4902099e01dd272d01cae71aba
zhou <2080211280@qq.com>
2020-04-01 15:29:09 +0800

    初始化碎片位置与plate组件分离

--- src/component/plate/Plate.js
+++ src/component/plate/Plate.js
@@ -3,37 +3,20 @@ import React from 'react'
 import './plate.css'
 
 
-
-const Plate = ({ url, xnum, ynum, handleTouchStart, handleTouchMove }) => {
-    var index = 0
-
-
-
+const Plate = ({ patchs, handleTouchStart, handleTouchMove, handleTouchEnd }) => {
     return (
         <div className='plate'>
             {
-                Array(xnum).fill().map((xitem, x) =>
-                    Array(ynum).fill().map((yitem, y) => {
-
-                        let tempStyle = {
-                            width: `${96 / xnum}%`, // 100 - 4个border
-                            height: `${96 / ynum}%`,
-                            backgroundImage: `url(${url})`,
-                            backgroundPosition: `${-y * 79.6 / xnum}vw ${-x * 79.6 / ynum}vw` // 80 - 0.4的border
-                        }
-
-                        return (
-                            <div key={++index}
-                                className='patch'
-                                style={tempStyle}
-                                onTouchStart={handleTouchStart}
-                                onTouchMove={handleTouchMove}
-                            >
-                                {index}
-                            </div>
-                        )
-                    })
-                )
+                patchs.map((patch, index) =>
+                    <div key={index}
+                        className='patch'
+                        style={patch.style}
+                        onTouchStart={handleTouchStart}
+                        onTouchMove={handleTouchMove}
+                        onTouchEnd={handleTouchEnd}
+                    >
+                        {index}
+                    </div>)
             }
         </div>
     )
--- src/game.js
+++ src/game.js
@@ -3,8 +3,10 @@ import Plate from './component/plate/Plate'
 import Layer from './component/layer/Layer'
 
 
-var xstart, ystart  // 点击开始位置
-var _style
+var xstart, ystart,  // 点击开始位置
+    layerStart,
+    layerLatest,
+    _patchs         // 碎片位置
 
 const Game = () => {
 
@@ -13,9 +15,28 @@ const Game = () => {
     const xnum = 3, ynum = 3
     const url = "http://www.zhousb.cn/upload/jagsaw/1.jpg"
 
-    const [style, setStyle] = useState(null)
+
+    const [level, setLevel] = useState(1)
+    const [layerStyle, setLayerStyle] = useState(null)
+    const [patchs, setPatchs] = useState(null)
+
 
     function handleTouchStart({ target, changedTouches }) {
+
+        // 初始化碎片位置
+        if (!_patchs) {
+            _patchs = []
+            for (var patch of target.parentNode.children) {
+                let { top, left, width, height } = patch.getBoundingClientRect()
+                _patchs.push({
+                    top,
+                    left,
+                    width,
+                    height
+                })
+            }
+        }
+
 
         // 只有一根手指
         if (1 !== changedTouches.length) {
@@ -24,10 +45,10 @@ const Game = () => {
         xstart = changedTouches[0].pageX
         ystart = changedTouches[0].pageY
 
-        let { top, left, width, height } = target.getBoundingClientRect()
-        const { backgroundImage, backgroundPosition } = target.style
 
-        setStyle(_style = {
+        let { top, left, width, height } = target.getBoundingClientRect()
+        let { backgroundImage, backgroundPosition } = target.style
+        setLayerStyle(layerStart = {
             top,
             left,
             width,
@@ -37,36 +58,86 @@ const Game = () => {
         })
     }
 
-    function handleTouchMove({ target, changedTouches }) {
+    function handleTouchMove({ changedTouches }) {
         if (1 !== changedTouches.length) {
             return false
         }
 
         let { pageX: xnow, pageY: ynow } = changedTouches[0]
-        let newStyle = copy(_style)
-        newStyle.top = ynow - (ystart - _style.top)
-        newStyle.left = xnow - (xstart - _style.left)
-        setStyle(newStyle)
+        layerLatest = copy(layerStart)
+        layerLatest.top = ynow - (ystart - layerStart.top)
+        layerLatest.left = xnow - (xstart - layerStart.left)
+        setLayerStyle(layerLatest)
     }
 
-    const plate = useMemo(() =>
-        <Plate
-            xnum={xnum}
-            ynum={ynum}
-            url={url}
+    function handleTouchEnd() {
+
+        let { top: y, left: x } = layerLatest
+
+        _patchs.map((patch) => {
+            let { top, left } = patch,
+                bottom = top + patch.height,
+                right = left + patch.width
+
+            if (left < x && right > x && top < y & bottom > y) {
+                console.log(patch)
+            }
+        })
+
+
+
+
+
+
+
+    }
+
+
+    useMemo(() => {
+        let patchs = [],
+            width = `${96 / xnum}%`, // 100 - 4个border
+            height = `${96 / ynum}%`,
+            backgroundImage = `url(${url})`
+
+        // 初始化碎片
+        Array(xnum).fill().map((xitem, x) =>
+            Array(ynum).fill().map((yitem, y) => {
+                patchs.push({
+                    style: {
+                        width,
+                        height,
+                        backgroundImage,
+                        backgroundPosition: `${-y * 79.6 / xnum}vw ${-x * 79.6 / ynum}vw` // 80 - 0.4的border
+                    }
+                })
+                return null
+            })
+        )
+        setPatchs(patchs)
+    }, [level])
+
+
+
+
+    const plate = useMemo(() => {
+        return (<Plate
+            patchs={patchs}
             handleTouchStart={handleTouchStart}
             handleTouchMove={handleTouchMove}
-        />, [xnum, ynum, url])
+            handleTouchEnd={handleTouchEnd}
+        />)
+    }, [patchs]),
 
+        layer = useMemo(() =>
+            <Layer style={layerStyle} />
+            , [layerStyle])
 
     return (
         <>
             {plate}
-            <Layer style={style} />
+            {layer}
         </>
     )
-
-
 }
 
 function copy(obj) {
